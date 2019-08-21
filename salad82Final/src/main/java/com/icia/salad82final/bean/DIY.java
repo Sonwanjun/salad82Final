745e822e9389e78fad0386b19911da5dae60314f
yeawon kim <kim8592635@gmail.com>
2019-08-21 19:19:45 +0900

    작성자:김예원 작업내용:P&D와 DIY상품 빈에 저장 작성일자:19/08/21

---
+++ salad82Final/src/main/java/com/icia/salad82final/bean/DIY.java
@@ -0,0 +1,8 @@
+package com.icia.salad82final.bean;
+
+public class DIY {
+	
+	private int p_code;
+	private int cf_code;
+	private int cs_code;
+}
---
+++ salad82Final/src/main/java/com/icia/salad82final/bean/PD.java
@@ -0,0 +1,17 @@
+package com.icia.salad82final.bean;
+
+import java.util.Date;
+
+public class PD {
+
+	private int p_code;
+	private String p_type;
+	private String p_name;
+	private String s_id;
+	private int  p_price;
+	private Date p_date;
+	private int p_selled;
+	private int p_remain;
+	private String  p_contents;
+}
+
