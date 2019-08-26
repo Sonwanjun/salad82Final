bf220305ac41fd353a13c3223cab0552327f6275
dltjscjswoek <dltjscjswoek@naver.com>
2019-08-26 14:36:40 +0900

    no message

--- salad82Final/src/main/java/com/icia/salad82final/SellerController.java
+++ salad82Final/src/main/java/com/icia/salad82final/SellerController.java
@@ -1,5 +1,6 @@
 package com.icia.salad82final;
 
+import java.util.List;
 import java.util.Locale;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +8,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.icia.salad82final.bean.Seller;
@@ -27,35 +30,10 @@ public class SellerController {
 		
 		return "sMyPage";
 	}
-	@RequestMapping(value = "/ingredient", method = RequestMethod.GET)  //재료관리
-	public String ingredient(Locale locale, Model model) {	
-		
-		return "ingredient";
-	}
-	@RequestMapping(value = "/myproductList", method = RequestMethod.GET)  //상품관리
-	public String myproductList(Locale locale, Model model) {	
-		
-		return "myproductList";
-	}
-	@RequestMapping(value = "/productStatus", method = RequestMethod.GET)  //주문현황
-	public String productStatus(Locale locale, Model model) {	
-		
-		return "productStatus";
-	}
-	@RequestMapping(value = "/productSale", method = RequestMethod.GET)  //판매실적
-	public String productSale(Locale locale, Model model) {	
-		
-		return "productSale";
-	}
-	@RequestMapping(value = "/requestHome", method = RequestMethod.GET)  //재료요청
-	public String requestHome(Locale locale, Model model) {	
-		
-		return "requestHome";
-	}
 	@RequestMapping(value = "/sellerInsert")
 	public ModelAndView sellerInsert(Seller st) {
 		mav=sm.sellerInsert(st);
 		return mav;
 	}
-			
+
 }
